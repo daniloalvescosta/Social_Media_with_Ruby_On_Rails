38784286623e357eed0d0dd66a343f54fd75938d
Danilo Costa <63356168+daniloalvescosta@users.noreply.github.com>
2022-06-30 09:35:19 -0300

    "destroy" bug fix
    I fixed the bug on the destroy system

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+
+RSpec.describe User, type: :model do
+   it 'should make a new user' do
+        user = User.new(email: 'danilo@email.com', password: '12345667886', password_confirmation: '12345667886')
+
+        expect(user).to be_valid
+    end
+end
